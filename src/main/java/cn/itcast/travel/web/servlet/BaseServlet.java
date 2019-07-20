79bc2b778b1f712a2aaa1a94def93646a5f3848b
Capriciousness <qq513819387@outlook.com>
2019-07-20 16:45:04 +0800

    抽取BaseServlet第一步

---
+++ src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
@@ -0,0 +1,42 @@
+package cn.itcast.travel.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class BaseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // System.out.println("BaseServlet的service方法被执行...");
+        //完成方法分发
+        //1.获取请求路径
+        String uri = req.getRequestURI();
+        System.out.println("请求uri:"+uri);
+        //2.获取方法名称
+        String methodName = uri.substring(uri.lastIndexOf('/') + 1);
+        System.out.println("方法名称:"+methodName);
+        //3.获取方法对象Method
+        System.out.println(this);//cn.itcast.travel.web.servlet.UserServlet@38ad0b1f
+        try {
+            ////忽略方法的访问权限不妥，私有方法也会被访问
+            Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            //4.执行方法
+            //暴力反射不妥
+            //method.setAccessible(true);
+            method.invoke(this,req,resp);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
