772654c241fbe304cb2b192a38868567e78412cb
Capriciousness <qq513819387@outlook.com>
2019-07-22 17:10:57 +0800

    修正了一个小错误，CategoryDaoImpl写错名字

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,6 @@
 package cn.itcast.travel.service.impl;
 
 import cn.itcast.travel.dao.CategoryDao;
-import cn.itcast.travel.dao.impl.CategoryDaoImpl;
 import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
 
