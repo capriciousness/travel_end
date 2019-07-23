ae237d8e085e508e2a67ae52e000e05082e4a17b
Capriciousness <qq513819387@outlook.com>
2019-07-23 15:11:33 +0800

    分类条目使用redis优化

--- src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import cn.itcast.travel.dao.CategoryDao;
 import cn.itcast.travel.dao.impl.CategoryDaoImpl;
 import cn.itcast.travel.domain.Category;
 import cn.itcast.travel.service.CategoryService;
+import cn.itcast.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CategoryServiceImpl implements CategoryService {
     CategoryDao categoryDao = new CategoryDaoImpl();
@@ -15,7 +19,31 @@ public class CategoryServiceImpl implements CategoryService {
      */
     @Override
     public List<Category> findAll() {
-
-        return categoryDao.findAll();
+        //1.从redis中查询
+        //1.1获取jedis客户端
+        Jedis jedis = JedisUtil.getJedis();
+        //1.2可使用sortedset排序查询
+        Set<String> categorys = jedis.zrange("category", 0, -1);
+        List<Category> cs = null;
+        //2.判断查询集合是否为空
+        if(categorys==null || categorys.size()==0){
+            //3.若为空，则从数据库中查询，存到redis中
+            System.out.println("查询数据库...");
+            cs = categoryDao.findAll();
+            for (int i = 0; i < cs.size(); i++) {
+                jedis.zadd("category",cs.get(i).getCid(),cs.get(i).getCname());
+            }
+        }else{
+            //4.若不为空，则将set数据存到list
+            System.out.println("从redis中查询...");
+            cs = new ArrayList<Category>();
+            for (String name : categorys) {
+                Category category = new Category();
+                category.setCname(name);
+                cs.add(category);
+            }
+            //System.out.println(cs);
+        }
+        return cs;
     }
 }
