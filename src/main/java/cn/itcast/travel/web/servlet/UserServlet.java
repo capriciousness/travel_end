5b85d0c51cb1e4e6e2baf166f2c8e270af320763
Capriciousness <qq513819387@outlook.com>
2019-07-24 17:13:10 +0800

    什么都没改

--- src/main/java/cn/itcast/travel/web/servlet/UserServlet.java
+++ src/main/java/cn/itcast/travel/web/servlet/UserServlet.java
@@ -20,10 +20,11 @@ import java.util.Map;
 @WebServlet("/user/*")
 public class UserServlet extends BaseServlet {
     //声明UserService的业务对象
-    private  UserService  service = new UserServiceImpl();
+    private UserService service = new UserServiceImpl();
 
     /**
      * 注册功能
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -31,7 +32,7 @@ public class UserServlet extends BaseServlet {
      */
     public void regist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //0.验证校验码
-        checkCode(request,response);
+        checkCode(request, response);
 
         //1.获取数据
         Map<String, String[]> map = request.getParameterMap();
@@ -39,7 +40,7 @@ public class UserServlet extends BaseServlet {
         //2.封装对象
         User user = new User();
         try {
-            BeanUtils.populate(user,map);
+            BeanUtils.populate(user, map);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
@@ -51,10 +52,10 @@ public class UserServlet extends BaseServlet {
         boolean flag = service.regist(user);
         ResultInfo info = new ResultInfo();
         //4.响应结果
-        if(flag){
+        if (flag) {
             //注册成功
             info.setFlag(true);
-        }else{
+        } else {
             //注册失败
             info.setFlag(false);
             info.setErrorMsg("注册失败!");
@@ -68,11 +69,12 @@ public class UserServlet extends BaseServlet {
         //设置content-type
         response.setContentType("application/json;charset=utf-8");
         response.getWriter().write(json);*/
-        writeValueAsString(info,response);
+        writeValueAsString(info, response);
     }
 
     /**
      * 登录功能
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -80,50 +82,51 @@ public class UserServlet extends BaseServlet {
      */
     public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //0.校验验证码
-        checkCode(request,response);
+        checkCode(request, response);
         //1.根据用户名和密码获取用户信息
         Map<String, String[]> map = request.getParameterMap();
         //2.封装数据
         User user = new User();
         try {
-            BeanUtils.populate(user,map);
+            BeanUtils.populate(user, map);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
         //3.调用service完成查询
-       // UserService service = new UserServiceImpl();
+        // UserService service = new UserServiceImpl();
         User u = service.login(user);
         ResultInfo info = new ResultInfo();
         //4.u是否存在
-        if(u == null){
+        if (u == null) {
             //用户不存在
             info.setFlag(false);
             info.setErrorMsg("用户名或密码错误!");
         }
         //5.用户是否激活
-        else if(!"Y".equals(u.getStatus())){
+        else if (!"Y".equals(u.getStatus())) {
             //用户未激活
             info.setFlag(false);
             info.setErrorMsg("该用户尚未激活!");
         }
         //6.判断登录成功
-        else if("Y".equals(u.getStatus())){
+        else if ("Y".equals(u.getStatus())) {
             //登录成功
             info.setFlag(true);
-            request.getSession().setAttribute("user",u);
+            request.getSession().setAttribute("user", u);
             //info.setErrorMsg("登录成功");
         }
         //7.响应数据
         /*ObjectMapper mapper = new ObjectMapper();
         response.setContentType("application/json;charset=utf-8");
         mapper.writeValue(response.getOutputStream(),info);*/
-        writeValue(info,response);
+        writeValue(info, response);
     }
 
     /**
      * 激活用户
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -132,16 +135,16 @@ public class UserServlet extends BaseServlet {
     public void active(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //1.获取激活码
         String code = request.getParameter("code");
-        if(code != null){
+        if (code != null) {
             //2.调用service完成激活
             UserService service = new UserServiceImpl();
             boolean flag = service.active(code);
             //3.判断标记
             String msg = null;
-            if(flag){
+            if (flag) {
                 //激活成功
                 msg = "激活成功，请<a href='login.html'>登录</a>";
-            }else{
+            } else {
                 //激活失败
                 msg = "激活失败，请重试！";
             }
@@ -154,6 +157,7 @@ public class UserServlet extends BaseServlet {
 
     /**
      * 返回姓名
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -166,11 +170,12 @@ public class UserServlet extends BaseServlet {
         /*ObjectMapper mapper = new ObjectMapper();
         response.setContentType("application/json;charset=utf-8");
         mapper.writeValue(response.getOutputStream(),user);*/
-        writeValue(user,response);
+        writeValue(user, response);
     }
 
     /**
      * 退出功能
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -180,7 +185,7 @@ public class UserServlet extends BaseServlet {
         //销毁session
         request.getSession().invalidate();
         //跳转到登录页面
-        response.sendRedirect(request.getContextPath()+"/login.html");
+        response.sendRedirect(request.getContextPath() + "/login.html");
     }
 
 }
